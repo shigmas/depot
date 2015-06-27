ab8678cb5efba484d920a5504e5c600b410a62b1
Masa <wyan.git@futomen.net>
2015-06-27 13:13:29 -0700

    More and updated command line args: target is now sdk arch is added

--- scripts/XcodeDriver.py
+++ scripts/XcodeDriver.py
@@ -5,35 +5,40 @@ import os, sys
 import subprocess
 
 class XcodeDriver:
-    BaseDirKey  = 'baseDir'
     ProjectsKey = 'projects'
 
     ProjectNameKey   = 'name'
     ProjectSchemeKey = 'scheme'
+    ProjectTypeKey   = 'type'
     ProjectSDKKey    = 'sdk'
 
-    def __init__(self, config):
-        self.baseDir = None
-        if config.has_key(self.BaseDirKey):
-            self.baseDir = config[self.BaseDirKey]
-        else:
-            print('XCODE_DRIVER: Missing %s' % self.BaseDirKey)
+    def __init__(self, config, baseDir, sdk, arch, isRelease=False):
+        self.baseDir = baseDir
+        self.isRelease = isRelease
+        self.sdk = sdk
+        self.arch = arch
+
         self.projects = []
         if config.has_key(self.ProjectsKey):
             self.projects = config[self.ProjectsKey]
             if not self._verifyProjects(self.projects):
                 print('XCODE_DRIVER: Projects key misconfigured')
         else:
-            print('XCODE_DRIVER: Missing %s' % self.BaseDirKey)
+            print('XCODE_DRIVER: Missing %s' % self.ProjectsKeys)
 
     def _verifyProjects(self, projects):
         lastName = None
         misconfigure = False
         for proj in projects:
-            if not proj.has_key(self.ProjectNameKey) or \
-            not proj.has_key(self.ProjectSchemeKey) or \
-            not proj.has_key(self.ProjectSDKKey):
-                print('XCODE_DRIVER: misconfiguration on or after %s' % lastName)
+            if not proj.has_key(self.ProjectNameKey):
+                print('XCODE_DRIVER: missing %s after %s' % (self.ProjectNameKey,
+                                                             lastName))
+                misconfigure = True
+            if not proj.has_key(self.ProjectSchemeKey) and \
+               not proj.has_key(self.ProjectTypeKey):
+                print('XCODE_DRIVER: missing %s and %s after %s' % (self.ProjectSchemeKey,
+                                                                    self.ProjectTypeKey,
+                                                                    lastName))
                 misconfigure = True
             else:
                 lastName = proj[self.ProjectNameKey]
@@ -57,11 +62,30 @@ class XcodeDriver:
 
     def _runBuild(self, projectArgs):
         projDir = os.path.join(self.baseDir, projectArgs[self.ProjectNameKey])
-        popenArgs = ['xcodebuild',
-                     '-project', os.path.expanduser(projDir),
-                     '-scheme',  projectArgs[self.ProjectSchemeKey],
-                     '-sdk',     projectArgs[self.ProjectSDKKey]]
-        print('XCODE_DRIVER: Running build for %s' % projDir)
+
+        # use scheme if present, else, use type with the release keyword.
+        if projectArgs.has_key(self.ProjectSchemeKey):
+            scheme = projectArgs[self.ProjectSchemeKey]
+        else:
+            if self.isRelease:
+                scheme = 'Release%s' % projectArgs[self.ProjectTypeKey]
+            else:
+                scheme = projectArgs[self.ProjectTypeKey]
+
+        
+        if self.sdk:
+            popenArgs = ['xcodebuild',
+                         '-project', os.path.expanduser(projDir),
+                         '-scheme',  scheme,
+                         '-arch',    self.arch,
+                         '-sdk',     self.sdk]
+        else:
+            popenArgs = ['xcodebuild',
+                         '-project', os.path.expanduser(projDir),
+                         '-scheme',  scheme,
+                         '-arch',    self.arch,
+                         '-sdk',     self.sdk]
+        print('XCODE_DRIVER: Running build for %s with args %s' % (projDir,popenArgs))
         result = 0
         try:
             result = subprocess.check_call(popenArgs)
--- scripts/build.py
+++ scripts/build.py
@@ -1,47 +1,177 @@
 #!/usr/bin/env python
 
+"""
+  build.py <parameters>
+
+  Script to build a series of xcode projects.  the config specifies the
+  projects, with the name being the xcode project based of a base directory
+  which is passed in on the command line.
+
+  parameters:
+  -b, --base [dir] (required)
+     base directory.  Full path to the root of the projects directory
+
+  -r, --release
+     If this is a release build.  Default is no
+
+  -s, --sdk
+     The sdk that we pass on to the build.  'xcodebuild -showsdks' to see list
+
+  -a, --arch
+     The architecture we pass on to the build.  'xcodebuild -showsdks' to see list
+
+  -?, --help
+     This message
+
+"""
+import os, sys
+import getopt
+
 # Easiest way to have a config is with a dictionary.  And since python
 # dictionaries are human readable, we just make this python file the config
 # file
 
+DefaultSDK = 'iphonesimulator8.3'
+DefaultArch = 'x86_64'
+
+#DefaultSDK = 'iphoneos8.3'
+#DefaultArch = 'arm7'
+
 # Since we generally build one set of these at a time
-FrameworkScheme = 'ReleaseFramework'
-ResourcesScheme = 'ReleaseResources'
-Sdk = 'iphoneos8.2'
+FrameworkType = 'Framework'
+ResourcesType = 'Resources'
 
 config = {
-    'baseDir': '~/src/tree/futomen',
     'projects': [
         {
-            'name'   : 'core/FFKit/FFKit.xcodeproj',
-            'scheme' : FrameworkScheme,
-            'sdk'    : Sdk,
+            'name' : 'libs/AFNetworking/AFNetworking.xcodeproj',
+            'type' : FrameworkType,
         },
         {
-            'name'   : 'core/FUIKit/FUIKit.xcodeproj',
-            'scheme' : FrameworkScheme,
-            'sdk'    : Sdk,
+            'name' : 'libs/HRColorPicker/HRColorPicker.xcodeproj',
+            'type' : FrameworkType,
         },
         {
-            'name'   : 'core/FUIKit/FUIKit.xcodeproj',
-            'scheme' : ResourcesScheme,
-            'sdk'    : Sdk,
+            'name' : 'libs/SVProgressHUD/SVProgressHUD.xcodeproj',
+            'type' : FrameworkType,
         },
         {
-            'name'   : 'core/FGLKit/FGLKit.xcodeproj',
-            'scheme' : FrameworkScheme,
-            'sdk'    : Sdk,
+            'name' : 'libs/SVProgressHUD/SVProgressHUD.xcodeproj',
+            'type' : ResourcesType,
         },
         {
-            'name'   : 'core/FPrim/FPrim.xcodeproj',
-            'scheme' : FrameworkScheme,
-            'sdk'    : Sdk,
+            'name' : 'core/FFKit/FFKit.xcodeproj',
+            'type' : FrameworkType,
+        },
+        {
+            'name' : 'core/FANG/FANG.xcodeproj',
+            'type' : FrameworkType,
+        },
+        {
+            'name' : 'core/FLogKit/FLogKit.xcodeproj',
+            'type' : FrameworkType,
+        },
+        {
+            'name' : 'core/FUIKit/FUIKit.xcodeproj',
+            'type' : FrameworkType,
+        },
+        {
+            'name' : 'core/FUIKit/FUIKit.xcodeproj',
+            'type' : ResourcesType,
+        },
+        {
+            'name' : 'core/FGLKit/FGLKit.xcodeproj',
+            'type' : FrameworkType,
+        },
+        {
+            'name' : 'core/FPrim/FPrim.xcodeproj',
+            'type' : FrameworkType,
+        },
+        {
+            'name' : 'core/FCache/FCache.xcodeproj',
+            'type' : FrameworkType,
+        },
+        {
+            'name' : 'libs/GLUKit/GLUKit.xcodeproj',
+            'type' : FrameworkType,
+        },
+        {
+            'name' : 'libs/FAPKit/FAPKit.xcodeproj',
+            'type' : FrameworkType,
+        },
+        {
+            'name' : 'libs/FAPKit/FAPKit.xcodeproj',
+            'type' : ResourcesType,
+        },
+        {
+            'name'   : 'apps/ios/MaltMap/MaltMap.xcodeproj',
+            'scheme' : 'MaltMap',
         },
     ]
 }
 
+def usage(why=None):
+    if (why):
+        print('Error: %s'% why)
+    print __doc__
 
-import XcodeDriver
+def parseArgs(argv):
+    SHORT_ARGS = '?b:rt:a:s:'
+    LONG_ARGS = ['help','base:','release','arch:', 'sdk:']
+    isRelease = False
+    baseDir = None
+    arch = None
+    sdk = None
 
-s = XcodeDriver.XcodeDriver(config)
-s.build()
+    try:
+        arglist, args = getopt.getopt(argv[1:],SHORT_ARGS, LONG_ARGS)
+    except getopt.error, why:
+        usage(why)
+        sys.exit()
+
+    try:
+        for (field, val) in arglist:
+            if field == '--help' or field == '-?':
+                usage()
+                sys.exit()
+            if field == '--base' or field == '-b':
+                baseDir = val
+            elif field == '--release' or field == '-r':
+                isRelease = True
+            elif field == '--sdk' or field == '-s':
+                sdk = val
+            elif field == '--arch' or field == '-a':
+                arch = val
+
+    except ValueError:
+        usage('Invalid parameter %s' % field)
+        sys.exit()
+
+    return baseDir, isRelease, sdk, arch
+
+
+def main(args, stdout, environ):
+
+    baseDir, isRelease, sdk, arch = parseArgs(args)
+
+    # verify the arguments
+    if baseDir is None:
+        usage(why='base directory not specified')
+        sys.exit()
+
+    if sdk is None:
+        sdk = DefaultSDK
+    if arch is None:
+        arch = DefaultArch
+
+    import XcodeDriver
+    print('args to driver: base: %s release: %s, sdk: %s, arch: %s' % (baseDir,
+                                                                       isRelease,
+                                                                       sdk,
+                                                                       arch))
+
+    s = XcodeDriver.XcodeDriver(config, baseDir, sdk, arch, isRelease)
+    s.build()
+
+if __name__ == '__main__':
+    main(sys.argv, sys.stdout, os.environ)
